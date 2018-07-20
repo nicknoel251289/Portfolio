0288afbdcb7a75902121468cb2cbbede2437ed00
Nick Noel <nicknoel251289@gmail.com>
2018-07-20 17:10:38 -0400

    updated case studies page and case studies section on index

--- Scripts/index.js
+++ Scripts/index.js
@@ -84,7 +84,7 @@ $(document).ready(function(){
 
   //see my work! button on main page
   $(".btn").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
+    // Makes sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
